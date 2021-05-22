23d5d2400acf1142d402924084bb7c9e08a21459
Christopher Mullins <pantherakrugeri@live.com>
2021-05-22 11:08:14 -0500

    general cleanup and introduced state for abilities class

--- src/modules/characterbuilder/abilities/Abilities.js
+++ src/modules/characterbuilder/abilities/Abilities.js
@@ -1,99 +1,78 @@
 import React from "react";
 import Ability from "./components/Ability";
-//import AbilityData from "./AbilityData";
+
+
+const getAbilityData = [{
+	abilityName: "Strength",
+	abilityId: "strength-id",
+	abilityType: "number",
+	abilityValue: 0,
+},
+{
+	abilityName: "Dexterity",
+	abilityId: "dexterity-id",
+	abilityType: "number",
+	abilityValue: 0,
+},
+{
+	abilityName: "Constitution",
+	abilityId: "constitution-id",
+	abilityType: "number",
+	abilityValue: 0,
+},
+{
+	abilityName: "Intelligence",
+	abilityId: "intelligence-id",
+	abilityType: "number",
+	abilityValue: 0,
+},
+{
+	abilityName: "Wisdom",
+	abilityId: "wisdom-id",
+	abilityType: "number",
+	abilityValue: 0,
+},
+{
+	abilityName: "Charisma",
+	abilityId: "charisma-id",
+	abilityType: "number",
+	abilityValue: 0,
+}];
 
 class Abilities extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: this.getAbilityData(),
-		};
-        
-        this.getAbilityData = this.getAbilityData.bind(this);
-	}
+	state = {
+		abilityData: getAbilityData
+	};
 
 	componentDidMount() {
-        console.log(this.state.data)
-    }
+		this.setState({ abilityData: getAbilityData });
+		console.log(this.state.abilityData);
+	}	
 
-    getAbilityData() {
-		let abilities = [
-			{
-				abilityName: "Strength",
-				abilityId: "strength-id",
-				abilityType: "number",
-				abilityValue: 0,
-			},
-			{
-				abilityName: "Dexterity",
-				abilityId: "dexterity-id",
-				abilityType: "number",
-				abilityValue: 0,
-			},
-			{
-				abilityName: "Constitution",
-				abilityId: "constitution-id",
-				abilityType: "number",
-				abilityValue: 0,
-			},
-			{
-				abilityName: "Intelligence",
-				abilityId: "intelligence-id",
-				abilityType: "number",
-				abilityValue: 0,
-			},
-			{
-				abilityName: "Wisdom",
-				abilityId: "wisdom-id",
-				abilityType: "number",
-				abilityValue: 0,
-			},
-			{
-				abilityName: "Charisma",
-				abilityId: "charisma-id",
-				abilityType: "number",
-				abilityValue: 0,
-			},
-		];
-        console.log(abilities);
-        return abilities;
-	}
-
-    renderAbilities() {
-        const abilities = this.state.data;
-        abilities.forEach(function (item, index) {
-            let x = item.abilityName;
-            console.log(x);
-            //return(<div>x</div>);
-            return <li>x</li>
-        });
-
-        console.log();
-
-        //return [abilities[0].abilityName];
-
-        return (
-            <div>
-            <Ability
-                print
-                abilityName={abilities[0].abilityName}
-                abilityValue={abilities[0].abilityValue}
-            />
-            <Ability
-                print
-                abilityName={abilities[1].abilityName}
-                abilityValue={abilities[1].abilityValue}
-            />
-            </div>
-        )
-    }
+	abilityList = this.state.abilityData.map(({ abilityName, abilityId, abilityValue, ...Args }) => (
+		<li key={abilityId}>
+			<Ability
+				//print
+				abilityName={abilityName}
+				abilityValue={this.state.abilityData.abilityValue}
+				abilityType={Args.abilityType}
+			/>
+		</li>
+	));
 
 	render() {
 		return (
-            <div>
-                {this.renderAbilities()}
-            </div>
-        )
+			<div>
+				<div style={{fontWeight: 'bold', paddingTop: '25px'}}>
+					Abilities
+				</div>
+				<div>
+					<ul style={{listStyleType: 'none'}}>
+						{this.abilityList}
+					</ul>
+				</div>
+			</div>
+		)
 	}
 }
 
