6d97dab732540340bf3e75239c0501a7188ff93a
Christopher Mullins <pantherakrugeri@live.com>
2021-06-02 20:23:22 -0500

    Added Axios api call and general format

--- src/modules/characterbuilder/abilities/Abilities.js
+++ src/modules/characterbuilder/abilities/Abilities.js
@@ -1,79 +1,134 @@
 import React from "react";
 import Ability from "./components/Ability";
+import Axios from "axios";
 
-
-const getAbilityData = [{
-	abilityName: "Strength",
-	abilityId: "strength-id",
-	abilityType: "number",
-	abilityValue: 0,
-},
-{
-	abilityName: "Dexterity",
-	abilityId: "dexterity-id",
-	abilityType: "number",
-	abilityValue: 0,
-},
-{
-	abilityName: "Constitution",
-	abilityId: "constitution-id",
-	abilityType: "number",
-	abilityValue: 0,
-},
-{
-	abilityName: "Intelligence",
-	abilityId: "intelligence-id",
-	abilityType: "number",
-	abilityValue: 0,
-},
-{
-	abilityName: "Wisdom",
-	abilityId: "wisdom-id",
-	abilityType: "number",
-	abilityValue: 0,
-},
-{
-	abilityName: "Charisma",
-	abilityId: "charisma-id",
-	abilityType: "number",
-	abilityValue: 0,
-}];
+let abilityData = [
+  {
+    abilityName: "Strength",
+    abilityId: "strength-id",
+    abilityType: "number",
+    abilityValue: 0,
+  },
+  {
+    abilityName: "Dexterity",
+    abilityId: "dexterity-id",
+    abilityType: "number",
+    abilityValue: 0,
+  },
+  {
+    abilityName: "Constitution",
+    abilityId: "constitution-id",
+    abilityType: "number",
+    abilityValue: 0,
+  },
+  {
+    abilityName: "Intelligence",
+    abilityId: "intelligence-id",
+    abilityType: "number",
+    abilityValue: 0,
+  },
+  {
+    abilityName: "Wisdom",
+    abilityId: "wisdom-id",
+    abilityType: "number",
+    abilityValue: 0,
+  },
+  {
+    abilityName: "Charisma",
+    abilityId: "charisma-id",
+    abilityType: "number",
+    abilityValue: 0,
+  },
+];
 
 class Abilities extends React.Component {
-	state = {
-		abilityData: getAbilityData
-	};
+  state = {};
 
-	componentDidMount() {
-		this.setState({ abilityData: getAbilityData });
-		console.log(this.state.abilityData);
-	}	
+  componentDidMount() {
+    this.getAbilities();
+    console.log(localStorage.getItem("abilities"));
+    let persistedAbilities = localStorage.getItem("abilities");
 
-	abilityList = this.state.abilityData.map(({ abilityName, abilityId, abilityValue, ...Args }) => (
-		<li key={abilityId}>
-			<Ability
-				//print
-				abilityName={abilityName}
-				abilityValue={this.state.abilityData.abilityValue}
-				abilityType={Args.abilityType}
-			/>
-		</li>
-	));
+    if (persistedAbilities === null) {
+      abilityData.map(({ abilityName, ...Args }, index) =>
+        this.setState({ [abilityName]: 0 })
+      );
+    }
+    console.log(this.state);
+  }
 
-	render() {
-		return (
-			<div>
-				<div style={{fontWeight: 'bold', paddingTop: '25px'}}>
-					Abilities
-				</div>
-				<div>
-					<ul style={{listStyleType: 'none'}}>
-						{this.abilityList}
-					</ul>
-				</div>
-			</div>
-		)
-	}
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(this.state);
+    let persistedAbilities = localStorage.getItem("abilities");
+    localStorage.setItem("abilities", JSON.stringify(this.state));
+    console.log("persistedAbilities: ", JSON.parse(persistedAbilities));
+  }
+
+  getAbilities() {
+    let abilityData = [];
+    Axios.get("http://localhost:3000/api/abilities")
+      .then(function (response) {
+        // handle success
+        console.log(response);
+
+        abilityData = response.data.data.map(
+          ({ abilityName, abilityId, ...Args }, index) => (
+            <li key={abilityId}>
+              <Ability
+                //print
+                abilityName={abilityName}
+                abilityValue={0}
+                inputProps={{ min: 0 }}
+                abilityType={Args.abilityType}
+                onChange={(e) =>
+                  this.onAbilityChange(abilityName, e.target.value)
+                }
+              />
+            </li>
+          )
+        );
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        return abilityData;
+      });
+  }
+
+  onAbilityChange(ability, val) {
+    console.log("onAbilityChange was called");
+    this.setState({ [ability]: val });
+  }
+
+  abilityList = abilityData.map(
+    ({ abilityName, abilityId, abilityValue, ...Args }, index) => (
+      <li key={abilityId + index}>
+        <Ability
+          //print
+          abilityName={abilityName}
+          abilityValue={this.state.abilityValue}
+          inputProps={{ min: 0 }}
+          abilityType={Args.abilityType}
+          onChange={(e) => this.onAbilityChange(abilityName, e.target.value)}
+        />
+      </li>
+    )
+  );
+
+  render() {
+    return (
+      <div>
+        <div style={{ fontWeight: "bold", paddingTop: "25px" }}>
+          <h2>Abilities</h2>
+        </div>
+        <div>
+          <ul style={{ listStyleType: "none" }}>{this.abilityList}</ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Abilities;
