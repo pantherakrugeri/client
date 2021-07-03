a87d228c08c55bddec0f78767d97b9ab1b635aa7
Christopher Mullins <pantherakrugeri@live.com>
2021-07-02 22:56:20 -0500

    Converted to functional component

--- src/components/characterbuilder/abilities/Abilities.js
+++ src/components/characterbuilder/abilities/Abilities.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Ability from './Ability';
 import { faDiceD20 } from '@fortawesome/free-solid-svg-icons';
 import Spinner from '../../layout/Spinner';
@@ -15,97 +15,86 @@ const styles = (theme) => ({
 	},
 });
 
-class Abilities extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			abilities: this.abilitiesConfig,
-			loading: false,
-		};
-	}
+const Abilities = (props) => {
+	const [abilitiesConfig, setAbilitiesConfig] = useState([]);
+	const [abilities, setAbilities] = useState({});
+	const [loading, setLoading] = useState(false);
 
-	initialAbilitiesConfig = [];
-	abilitiesConfig = {};
+	useEffect(() => {
+		getAbilitiesConfig();
+	}, []);
 
-	async componentDidMount() {
+	useEffect(() => {
+		setInitialAbilities();
+	}, [abilitiesConfig]);
+
+	const getAbilitiesConfig = async () => {
 		try {
-			this.setState({ loading: true });
+			setLoading(true);
 			const res = await Axios.get('http://localhost:3000/api/abilities');
-			this.initialAbilitiesConfig = res.data.data;
-			this.abilityConfig(res.data);
-			this.setInitialAbilitiesState();
+			setAbilitiesConfig(res.data.data);
 		} catch (error) {
 			console.log(error);
 		} finally {
-			this.setState({ loading: false });
+			setLoading(false);
 		}
-	}
-
-	componentDidUpdate(prevProps, prevState, snapshot) {
-		//console.log(prevState);
-		// let persistedAbilities = localStorage.getItem('abilities');
-		// localStorage.setItem('abilities', JSON.stringify(this.state));
-		// console.log('persistedAbilities: ', JSON.parse(persistedAbilities));
-	}
-
-	onAbilityChange = (e) => {
-		const { abilities } = { ...this.state }; // manage nested state
-		const currentState = abilities;
-		const { name, value } = e.target;
-		currentState[name] = value;
-
-		this.setState({ abilities: currentState });
 	};
 
-	setInitialAbilitiesState = () => {
-		this.initialAbilitiesConfig.forEach(function callbackFn(element, index) {
+	const setInitialAbilities = () => {
+		abilitiesConfig.forEach(function callbackFn(element, index) {
 			const abilityStateName = element.abilityName.toLowerCase();
-			this.abilitiesConfig[abilityStateName] = '';
+			abilities[abilityStateName] = '';
 		}, this);
 	};
 
-	abilityConfig = () =>
-		this.initialAbilitiesConfig.map(
-			({ abilityName, abilityId, abilityValue, ...Args }) => (
-				<li key={abilityId}>
-					<Ability
-						abilityId={abilityId.toString()}
-						abilityName={abilityName}
-						abilityValue={this.state.abilities[abilityName.toLowerCase()]}
-						inputProps={{ min: 0 }}
-						abilityType={Args.abilityType}
-						onChange={this.onAbilityChange}
-					/>
-				</li>
-			)
+	const onAbilityChange = (e) => {
+		const { name, value } = e.target;
+
+		setAbilities({
+			...abilities,
+			[name]: value,
+		});
+	};
+
+	const abilityFields = abilitiesConfig.map(
+		({ abilityName, abilityId, ...Args }) => (
+			<li key={abilityId}>
+				<Ability
+					abilityId={abilityId.toString()}
+					abilityName={abilityName}
+					abilityType={Args.abilityType}
+					abilityValue={abilities[abilityName]}
+					inputProps={{ min: 0 }}
+					onChange={onAbilityChange}
+				/>
+			</li>
+		)
+	);
+
+	const { classes } = props;
+
+	if (loading) {
+		return (
+			<div>
+				<Overlay />
+				<Spinner
+					spinnerIcon={faDiceD20}
+					iconSize='3x'
+					iconColor='gold'
+					iconClasses='fas vm-align'
+					spinnerText='Loading...'
+					spanClasses='spin-span x-large primeColor'
+				/>
+			</div>
 		);
-
-	render() {
-		const { classes } = this.props;
-
-		if (this.state.loading) {
-			return (
-				<div>
-					<Overlay />
-					<Spinner
-						spinnerIcon={faDiceD20}
-						iconSize='3x'
-						iconColor='gold'
-						iconClasses='fas vm-align'
-						spinnerText='Loading...'
-						spanClasses='spin-span x-large primeColor'
-					/>
-				</div>
-			);
-		} else {
-			return (
-				<div className={classes.root}>
-					<h3>{this.props.title}</h3>
-					<ul style={{ listStyleType: 'none' }}>{this.abilityConfig()}</ul>
-				</div>
-			);
-		}
+	} else {
+		return (
+			<div className={classes.root}>
+				<h3>{props.title}</h3>
+				<ul style={{ listStyleType: 'none' }}>{abilityFields}</ul>
+			</div>
+		);
 	}
-}
+};
 
 export default withStyles(styles, { withTheme: true })(Abilities);
